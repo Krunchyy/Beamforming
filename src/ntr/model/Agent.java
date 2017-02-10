485efc2558694f22ea24508ec5c11fc99ea4d4a2
Tanaky <charp.antoine@gmail.com>
2017-02-10 17:59:44 +0100

    Comment to use the Queue interface in Agent.java

--- src/ntr/model/Agent.java
+++ src/ntr/model/Agent.java
@@ -1,6 +1,7 @@
 package ntr.model;
 
 import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
 
 import ntr.environement.Environement;
@@ -14,6 +15,8 @@ public class Agent extends Model{
 	public Agent(Location loc, Environement env)
 	{
 		super(loc, env);
+		Queue<Paquet> q = new ArrayBlockingQueue<>(255);
+		//TODO: utilisation de la Queue (utiliser la méthode add -> lève une exception quand la Queue est pleine)
 	}
 	
 	@Override
