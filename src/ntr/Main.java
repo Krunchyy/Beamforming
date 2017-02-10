122f034fbc19c1f2e83fac8863d6a643469ac51c
Tanaky <charp.antoine@gmail.com>
2017-02-10 17:48:33 +0100

    What to do in the first step

--- src/ntr/Main.java
+++ src/ntr/Main.java
@@ -12,6 +12,7 @@ public class Main {
 	public static final int SIZE = 5;
 	public static void main(String[] args)
 	{
+		//TODO: Une antenne, un Mobile, un générateur de paquet, un ordonnanceur
 		Environement env = new Environement(SIZE);
 		
 		IModel m1 = new Mobile(new Location(0,0), env);//auto env reg
