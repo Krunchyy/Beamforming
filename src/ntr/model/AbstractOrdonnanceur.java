57b2519bb3260e47bb079e6141de30e705504c9b
Tanaky <charp.antoine@gmail.com>
2017-02-17 16:34:05 +0100

    Recommit RoundRobin

--- src/ntr/model/AbstractOrdonnanceur.java
+++ src/ntr/model/AbstractOrdonnanceur.java
@@ -3,17 +3,22 @@ package ntr.model;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentHashMap;
 
+import ntr.signal.OFDM;
 import ntr.signal.Packet;
 
 public abstract class AbstractOrdonnanceur {
+	private OFDM ofdm;
+	
 	private ConcurrentHashMap<IModel, Queue<Packet>> map;
 	
 	/**
 	 * All ordonnanceurs event Opportunistic one should have access to the map in parameters
 	 * @param map
+	 * @param ofdm 
 	 */
-	public AbstractOrdonnanceur(ConcurrentHashMap<IModel, Queue<Packet>> map) {
+	public AbstractOrdonnanceur(ConcurrentHashMap<IModel, Queue<Packet>> map, OFDM ofdm) {
 		this.map = map;
+		this.ofdm = ofdm;
 	}
 	
 	public ConcurrentHashMap<IModel, Queue<Packet>> getMap() {
@@ -30,4 +35,8 @@ public abstract class AbstractOrdonnanceur {
 	 * @return le terminal qui va recevoir ses datas.
 	 */
 	public abstract IModel choose();
+	
+	public OFDM getOfdm() {
+		return this.ofdm;
+	}
 }
--- src/ntr/model/RoundRobin.java
+++ src/ntr/model/RoundRobin.java
@@ -1,21 +1,61 @@
 package ntr.model;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map.Entry;
 import java.util.Queue;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
+import ntr.signal.OFDM;
 import ntr.signal.Packet;
 
 
 public class RoundRobin extends AbstractOrdonnanceur {
-
-	public RoundRobin(ConcurrentHashMap<IModel, Queue<Packet>> map) {
-		super(map);
+	private IModel lastModel;
+	public RoundRobin(ConcurrentHashMap<IModel, Queue<Packet>> map, OFDM ofdm) {
+		super(map, ofdm);
+		this.lastModel = null;
 	}
 
 	@Override
-	public void tick() {
-		// TODO Auto-generated method stub
-
+	public void tick() { 
+		Set<Entry<IModel, Queue<Packet>>> entryset = this.getMap().entrySet();
+		
+		Iterator<Entry<IModel, Queue<Packet>>> iter = entryset.iterator();
+		boolean lastModelFound = false;
+		
+		while(iter.hasNext()) {
+			Entry<IModel, Queue<Packet>> entry = iter.next();
+			
+			if(this.lastModel != null && this.getMap().containsKey(this.lastModel)) {
+				if(lastModelFound) {
+					this.allow(entry.getKey());
+					return;
+				}
+				if(entry.getKey() == this.lastModel) {
+					lastModelFound = true;
+				}
+			}
+			else {
+				this.allow(entry.getKey());
+			}
+		}
+	}
+	
+	private void allow(IModel model) {
+		this.lastModel = model;
+		
+		Queue<Packet> buffer = this.getMap().get(model);
+		
+		int slot = this.getOfdm()._currentIndex;;//slot ?
+		ArrayList<Packet> packets = new ArrayList<>();
+		for(int i=0 ; i< this.getOfdm()._nb_sub_carrier ; i++) {
+			if(!buffer.isEmpty())
+				packets.add(buffer.poll());
+		}
+		
+		this.getOfdm().setTimeSlot(slot, (Packet[]) packets.toArray());
 	}
 
 	@Override
