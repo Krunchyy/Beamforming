ceda7ab1efb3a0001235356e253f77f79aa6b4ae
Di-0x <di0x@protonmail.com>
2017-02-28 11:13:08 +0100

    Add basic method for multipathfading

--- src/ntr/environement/pertubation/MultiPathFinding.java
+++ src/ntr/environement/pertubation/MultiPathFinding.java
@@ -4,8 +4,9 @@ import java.util.List;
 
 import ntr.environement.Environement;
 import ntr.model.IModel;
+import ntr.model.Mobile;
 import ntr.signal.Alteration;
-
+import ntr.utils.RandomUtils;
 public class MultiPathFinding extends Alteration{
 
 
@@ -15,5 +16,12 @@ public class MultiPathFinding extends Alteration{
 		
 	}
 
+	public void multipathfadingAlteration (Mobile mobile){
+		int random = RandomUtils.get(-50, 51);
+		int network = mobile.getNetworkCondition();
+		network += random;
+		mobile.setNetworkCondition(network);
+		
+	}
 
 }
