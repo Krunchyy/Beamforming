492956e1445bd0e0d170ac9213204bf285959ba5
Krunchy <spamshieldbouh2@gmail.com>
2017-02-28 13:27:52 +0100

    [UPDATE:Config] remove final status, (can be edited by Modelisation class)

--- src/ntr/utils/Config.java
+++ src/ntr/utils/Config.java
@@ -2,18 +2,18 @@ package ntr.utils;
 
 public class Config {
 	//Shadowing config:
-	public final static int SHADOWING_UPATE_FREQUENCY = 5;
-	public final static int SHADOWING_MAXIMUM_ALTERATION = 15;
+	public static int SHADOWING_UPATE_FREQUENCY = 5;
+	public static int SHADOWING_MAXIMUM_ALTERATION = 15;
 	
 	//IModel config:
-	public final static char MOBILE_TAG = 'M';
-	public final static char AGENT_TAG = 'A';
+	public static char MOBILE_TAG = 'M';
+	public static char AGENT_TAG = 'A';
 	
 	//PacketFragment config:
-	public final static int PACKET_DATA_SIZE = 100;
+	public static int PACKET_DATA_SIZE = 100;
 	
 	
 	
 	//OFDM
-	public static final boolean OFDM_DEBUG = false;
+	public static boolean OFDM_DEBUG = false;
 }
