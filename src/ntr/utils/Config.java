a06eb585e482af5ec6d2ae0611ef2e24f3e71d93
Krunchy <spamshieldbouh2@gmail.com>
2017-02-28 10:47:44 +0100

    [CONF] OFDM add OFDM debug conf

--- src/ntr/utils/Config.java
+++ src/ntr/utils/Config.java
@@ -2,4 +2,12 @@ package ntr.utils;
 
 public class Config {
 	public final static int SHADOWING_UPATE_FREQUENCY = 5;
+	
+	
+	
+	/*******
+	 * OFDM
+	 *******/
+	
+	public static final boolean OFDM_DEBUG = false;
 }
