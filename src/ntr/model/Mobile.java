b17c1157074ae3dbb699df66757d69ab660f68ad
Thomas <10008185@e105m06.istic.univ-rennes1.fr>
2017-03-07 13:04:40 +0100

    update Mobile getSNR and computeSNR with timeslots

--- src/ntr/model/Mobile.java
+++ src/ntr/model/Mobile.java
@@ -12,12 +12,15 @@ public class Mobile extends Model{
 	
 	private int networkCondition;
 	
-	private ConcurrentHashMap<IModel, ArrayList<Double>> mkn_map;
+	private ConcurrentHashMap<IModel, ConcurrentHashMap<Integer, ArrayList<Double>>> _mknMap;
+		
+	private int currentTimeSlot;
 	
 	public Mobile(Location loc, Environement env)
 	{
 		super(loc, env);
 		this.networkCondition = 0;
+		this.currentTimeSlot = 0;
 	}	
 	
 	@Override
@@ -67,25 +70,34 @@ public class Mobile extends Model{
 		return _packetFlow;
 	}
 	
-	public double getSNR(Agent agent, int sub_carrier, int timeslot){
-		return mkn_map.get(agent).get(sub_carrier);
+	/**
+	 * Return the sub_carrier of a timeslot of the mobile connected to an agent
+	 * @param agent
+	 * @param sub_carrier
+	 * @param timeslot between 0 and 9
+	 * @return
+	 */
+	public double getSNR(Agent agent, int sub_carrier, int timeslot) {
+		return _mknMap.get(agent).get(timeslot%agent._ofdm._nb_time_slot).get(sub_carrier);
 	}
 	
 	/**
-	 * Il faut calculer le Mkn pour chaque subcarrier de la timeslot actuel.
-	 * @param agent : l'agent pour lequel il faut calculer le Mkn
+	 * Compute mkn pour every subcarrier of the current timeslot
+	 * @param agent
 	 */
 	public void computeSNR(Agent agent) {
+		currentTimeSlot = currentTimeSlot%agent._ofdm._nb_time_slot; // currentTimeSlot between 0 and nb_sub_carrier excluded
 		int nb_sub_carrier = agent._ofdm._nb_sub_carrier;
 		ArrayList<Double> list = new ArrayList<>();
 		
 		for(int i=0; i<nb_sub_carrier; i++){
 			double a = 1 + (3 * agent._diffusPower * RandomUtils.multitrajet() * Math.pow(2,(1/ RandomUtils.setDelta(agent, this))));
-			double mkn = Math.log10(a) / Math.log10(2); // log2( 1 + (3*puis*noisekn*(1/dist)²) )
+			double mkn = Math.log10(a) / Math.log10(2); // log2( a )
 			list.add(mkn);
 		}
-		
-		mkn_map.put(agent, list);
+		_mknMap.get(agent).put(currentTimeSlot, list);
+
+		currentTimeSlot++;
 		/*
 		 * dist: Le Mkn prend en compte l'éloignement par rapport à l'antenne,
 		 * noise: l'affaiblissement lie aux multi-trajets
