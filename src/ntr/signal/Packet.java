e3eb9f766afe41326c7bead2d33f4d4b8d469ebc
Tanaky <charp.antoine@gmail.com>
2017-03-17 17:45:02 +0100

    Update Packet definition

--- src/ntr/signal/Packet.java
+++ src/ntr/signal/Packet.java
@@ -3,14 +3,21 @@ package ntr.signal;
 import java.util.ArrayList;
 import java.util.List;
 
+import ntr.model.Agent;
+import ntr.model.Mobile;
 import ntr.utils.Config;
 
 public class Packet {
-	private int dateCreation, dateArrivee;
+	private Mobile receiver;
+	private Agent sender;
+	
+	private long dateCreation, dateArrivee;
 	private int sizeSend, size;
 	private List<PacketFragment> fragments;
 	
-	public Packet(int dateCreation) {
+	public Packet(Agent sender, Mobile receiver, int dateCreation) {
+		this.receiver = receiver;
+		this.sender = sender;
 		this.dateCreation = dateCreation;
 		this.size = Config.SIZE;
 		this.sizeSend = 0;
@@ -18,8 +25,33 @@ public class Packet {
 	}
 	
 	public void addFragment(PacketFragment fragment) {
-		fragment.
+		if (this.sizeSend >= this.size)
+			return;
+		
 		this.fragments.add(fragment);
 	}
 	
+	public int getSize() {
+		return this.size;
+	}
+	
+	public long getDateCreation() {
+		return dateCreation;
+	}
+
+	public long getDateArrivee() {
+		return dateArrivee;
+	}
+
+	public void setDateArrivee(long dateArrivee) {
+		this.dateArrivee = dateArrivee;
+	}
+
+	public int getSizeSend() {
+		return sizeSend;
+	}
+
+	public void setSizeSend(int sizeSend) {
+		this.sizeSend = sizeSend;
+	}
 }
